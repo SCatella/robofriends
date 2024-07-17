1318a29d62734f946b1b3e4c14015b8dd9846df8
SCatella <samuel.catella@gmail.com>
2024-07-17 17:00:47 -0600

    added fetch call for user information.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CardArray from '../components/CardArray';
 import SearchBox from "../components/SearchBox";
-import { robots } from "../robots";
 
 import './App.css';
 
@@ -10,21 +9,29 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
   }
 
   onSearchChange = (event) => {
     this.setState({searchField: event.target.value})
   }
+
   
   render() {
     const filteredRobots = this.state.robots.filter(robots => {
       return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
     })
 
-    return (
+    return (!this.state.robots.length) ? <h1>Loading</h1> :
+    (
       <div className="App tc">
         <header className="App-header">
           <h1 className='f1'>RoboFriends</h1>
@@ -34,6 +41,7 @@ class App extends Component {
       </div>
     );
   }
+
 }
 
 export default App;
